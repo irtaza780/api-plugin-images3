c388d09bcfe6fb04ac181113dc9d9000913bfb18
muhammadirtaza <muhammad.irtaza@codistan.org>
2023-05-01 19:39:03 +0500

    fix: signed url console

--- src/utils/signedUrls.js
+++ src/utils/signedUrls.js
@@ -1,8 +1,8 @@
 import AWS from "aws-sdk";
 AWS.config.update({
-    accessKeyId: process.env.ID,
-    secretAccessKey: process.env.SECRET,
-    region: process.env.REGION
+  accessKeyId: process.env.ID,
+  secretAccessKey: process.env.SECRET,
+  region: process.env.REGION,
 });
 const s3 = new AWS.S3();
 
@@ -10,12 +10,12 @@ const bucket = process.env.BUCKET_NAME;
 // const object_key = "public/user/test/large-1675672214390-9hbt8dzg0nt81.jpg";
 const expires = 3600;
 
-export default async function generateSignedUrl (object_key) {
-    const signedUrl = await s3.getSignedUrlPromise("getObject", {
-        Bucket: bucket,
-        Key: object_key,
-        Expires: expires
-    });
-    console.log("signedUrl", signedUrl, object_key)
-    return signedUrl;
+export default async function generateSignedUrl(object_key) {
+  const signedUrl = await s3.getSignedUrlPromise("getObject", {
+    Bucket: bucket,
+    Key: object_key,
+    Expires: expires,
+  });
+
+  return signedUrl;
 }
